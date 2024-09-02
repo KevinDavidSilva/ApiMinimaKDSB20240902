ff0da67517587309f0e50c6f8ecd50fe6cd8db6f
KevinSilva <kevinsilva462@gmail.com>
2024-09-02 11:51:41 -0600

    Api producto

---
+++ KDSB20230901/Producto.cs
@@ -0,0 +1,10 @@
+﻿namespace KDSB20230901
+{
+    public class Producto
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public int cantidad { get; set; }
+    }
+}
