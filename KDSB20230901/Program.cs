9773779053148d12fceecf61b0cfa138beae8aae
KevinSilva <kevinsilva462@gmail.com>
2024-09-02 11:56:41 -0600

    los endpoint terminados

--- KDSB20230901/Program.cs
+++ KDSB20230901/Program.cs
@@ -1,3 +1,5 @@
+using KDSB20230901;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,7 +18,60 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+var producto = new List<Producto>();
 
+//Get
+app.MapGet("/producto", () =>
+{
+    return producto;
+});
+
+app.MapGet("/producto/{id}", (int id) =>
+{
+    var product = producto.FirstOrDefault(c => c.Id == id);
+    return product;
+});
+
+//Post
+app.MapPost("/producto", (Producto producto1) =>
+{
+    producto.Add(producto1);
+    return Results.Ok();
+
+});
+
+//put
+app.MapPut("/producto/{id}", (int id, Producto product) =>
+{
+    var existingProduct = producto.FirstOrDefault(c => c.Id == id);
+    if (existingProduct != null)
+    {
+        existingProduct.Name = product.Name;
+        existingProduct.Price = product.Price;
+        existingProduct.cantidad = product.cantidad;
+        return Results.Ok();
+    }
+    else
+    {
+        return Results.NotFound();
+    }
+
+});
+
+//Delete
+app.MapDelete("/producto/{id}", (int id) =>
+{
+    var existingProduct = producto.FirstOrDefault(c => c.Id == id);
+    if (existingProduct == null)
+    {
+        producto.Remove(existingProduct);
+        return Results.Ok();
+    }
+    else
+    {
+        return Results.NotFound();
+    }
+});
 
 app.Run();
 
